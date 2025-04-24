8fbb3d4a39e8060d266b173fb38d5f4363696ded
hungsean <hungsean0419@gmail.com>
2025-04-24 12:58:08 +0800

    ✨ New project

---
+++ main.py
@@ -0,0 +1,11 @@
+from ultralytics import YOLO
+
+# 載入預訓練好的 YOLOv11n 模型（可換成 yolov11s.pt, yolov11m.pt 等）
+model = YOLO("yolo11m.pt")
+# model.to("cuda")
+
+# 對一張圖片進行物件偵測
+results = model("Makati_intersection.jpg")  # 替換成你實際的圖片路徑
+
+# 顯示結果（自帶繪製邊框與 label）
+results[0].show()
